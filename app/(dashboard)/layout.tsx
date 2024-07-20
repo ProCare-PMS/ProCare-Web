085ae771eef9e93eb3692b26981d7f827bc33a10
Aneal Laryea <aneallaryea100@gmail.com>
2024-07-20 14:23:42 +0000

    "Removed font imports and variables, updated layout and navigation, added redirect in next.config, and modified tailwind config"

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,31 +1,31 @@
 import MainNav from "@/components/Main-Nav/MainNav";
 import DashboardNav from "./_components/DashboardNav"
-import { Open_Sans, Roboto_Mono, Inter, Nunito_Sans } from 'next/font/google'
+// import { Open_Sans, Roboto_Mono, Inter, Nunito_Sans } from 'next/font/google'
 
-const openSans = Open_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  //👇 Add variable to our object
-  variable: '--font-opensans',
-})
+// const openSans = Open_Sans({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   //👇 Add variable to our object
+//   variable: '--font-opensans',
+// })
 
-const inter = Inter({ 
-  subsets: ["latin"],
-  variable: '--font-inter' ,
-});
+// const inter = Inter({ 
+//   subsets: ["latin"],
+//   variable: '--font-inter' ,
+// });
 
-const nunito_sans = Nunito_Sans({
-  subsets: ['latin'],
-  display: "swap",
-  variable: '--font-nunito-sans',
-})
+// const nunito_sans = Nunito_Sans({
+//   subsets: ['latin'],
+//   display: "swap",
+//   variable: '--font-nunito-sans',
+// })
 
-//👇 Configure the object for our second font
-const robotoMono = Roboto_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto-mono',
-})
+// //👇 Configure the object for our second font
+// const robotoMono = Roboto_Mono({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-roboto-mono',
+// })
 
 export default function DashboardLayout({
   children,
@@ -33,10 +33,17 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <body
-      className={`flex min-h-screen flex-col ${openSans.variable} ${nunito_sans.variable} ${inter.variable} ${robotoMono.variable}`}
-    >
+    // <body
+    //   className={`flex min-h-screen flex-col ${openSans.variable} ${nunito_sans.variable} ${inter.variable} ${robotoMono.variable}`}
+    // >
+    //   <MainNav />
+    //   <main className=" pt-24 bg-[#F5F5F5] ">{children}</main>{" "}
+    //   {/* bg-[#F5F5F5] */}
+    // </body>
+
+    <body>
       <MainNav />
+      <DashboardNav />
       <main className=" pt-24 bg-[#F5F5F5] ">{children}</main>{" "}
       {/* bg-[#F5F5F5] */}
     </body>
