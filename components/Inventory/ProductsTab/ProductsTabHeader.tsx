b93b71a1a82f16df7ee247b49d8e5f9714abd52e
Marvinkwame <marvinkudjo@gmail.com>
2024-07-21 19:33:03 +0000

    Minor Changes to border radius on dashboard table

--- components/Inventory/ProductsTab/ProductsTabHeader.tsx
+++ components/Inventory/ProductsTab/ProductsTabHeader.tsx
@@ -9,9 +9,13 @@ import AddProducts from "@/app/(dashboard)/inventory/_components/AddProductsModa
 import ImportProductsModal from "@/app/(dashboard)/inventory/_importProductsComponents/ImportProductsModal";
 import { Button } from "@/components/ui/button";
 import FilterDropdown from "@/components/DropDown/FilterDropDown";
+import { ExpandableDataTable } from "@/components/Tables/expandable-data-table";
+import { productsTabColumns } from "@/components/Tables/products-tab-columns";
+import { productsTabTable } from "@/type";
 
 const ProductsTabHeader = () => {
-  const [showTab, setShowTab] = useState(<ProductMiniTable />);
+  const [showTab, setShowTab] = useState(<ExpandableDataTable columns={productsTabColumns}
+    data={productsTabTable}  />);
   const [activeTab, setActiveTab] = useState("Products");
   const [showMenu, setShowMenu] = useState(false);
   const [showFilters, setShowFilter] = useState(false);
@@ -20,14 +24,19 @@ const ProductsTabHeader = () => {
     <div>
       <div className="flex items-center justify-between">
         {/* Tabs Section */}
-        <div className="flex items-center bg-[#858C95]/50 gap-6 p-1 rounded-[10px]">
+        <div className="flex items-center bg-[#858C95]/50 gap-6 p-1 rounded-[10px] mb-8">
           <button
             onClick={() => {
-              setShowTab(<ProductMiniTable />);
+              setShowTab(
+                <ExpandableDataTable
+                  columns={productsTabColumns}
+                  data={productsTabTable}
+                />
+              );
               setActiveTab("Products");
             }}
             className={clsx(
-              "px-5 py-1 text-base font-medium transition",
+              "px-5 py-1 text-base font-medium",
               activeTab === "Products"
                 ? "bg-white rounded-[8px] shadow-md text-[#0B2B23]"
                 : "text-gray-50"
@@ -53,7 +62,7 @@ const ProductsTabHeader = () => {
 
         {/* Search and Filter Section */}
         <div className="flex items-center gap-3">
-          <div className="flex flex-row border-2 border-[#EAEBF0] rounded-xl items-center flex-1 gap-2 bg-transparent p-1">
+          <div className="flex flex-row border-2 border-[#EAEBF0] rounded-[14px] items-center flex-1 gap-2 bg-transparent p-1">
             <CiSearch />
             <input
               type="search"
