30d33a5a36d180fea875171f9b9c9da75646ead3
Marvinkwame <marvinkudjo@gmail.com>
2024-08-15 18:30:41 +0000

    Login Flow Refactor

---
+++ components/user_passcode/otp-input.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const OtpInput = () => {
+  return (
+    <div className="flex items-center justify-between overflow-hidden">
+      <div>
+        <input
+          className="border-2 w-16 h-12 border-[#D0D5DD] rounded-[8px]"
+          type="text"
+          name="otp"
+          id=""
+        />
+      </div>
+      <div>
+        <input
+          className="border-2 w-16 h-12 border-[#D0D5DD] rounded-[8px]"
+          type="text"
+          name="otp"
+          id=""
+        />
+      </div>
+      <div>
+        <input
+          className="border-2 w-16 h-12 border-[#D0D5DD] rounded-[8px]"
+          type="text"
+          name="otp"
+          id=""
+        />
+      </div>
+      <div>
+        <input
+          className="border-2 w-16 h-12 border-[#D0D5DD] rounded-[8px]"
+          type="text"
+          name="otp"
+          id=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default OtpInput;
