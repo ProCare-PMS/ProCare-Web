7b1fdf71c388a4fc655d6634885931041870304c
Marvinkwame <marvinkudjo@gmail.com>
2024-12-03 16:25:50 +0000

    Suppliers and Purchase Modal Refactor

--- components/Inventory/PurchasesTab/AddPurchase/AddPurchase.tsx
+++ components/Inventory/PurchasesTab/AddPurchase/AddPurchase.tsx
@@ -6,7 +6,6 @@ interface AddPurchaseProps {
 }
 
 const AddPurchase = ({ onClose }: AddPurchaseProps) => {
-  
   // State to keep track of product forms
   const [products, setProducts] = useState([{ id: 1 }]);
 
@@ -19,121 +18,124 @@ const AddPurchase = ({ onClose }: AddPurchaseProps) => {
   };
 
   return (
-    <div className="bg-white shadow-custom py-4 px-8 mb-12 rounded-[8px] mt-8 grid gap-y-5 ">
-      <div className="flex items-center gap-4 mb-4">
-        <MoveLeft onClick={onClose} className="cursor-pointer" />
-        <h3 className="font-bold text-2xl font-inter">Add Purchase</h3>
-      </div>
-      <hr />
-      <div className="grid">
-        {/* Purchase Details */}
-        <div>
-          <h2 className="text-[#202224] font-bold text-base font-inter">
-            Purchase Details
-          </h2>
-          <form>
-            <div className="grid gap-y-2 mt-3">
-              <label
-                htmlFor=""
-                className="text-[#323539] font-inter font-medium text-sm"
-              >
-                Select Supplier
-              </label>
-              <select
-                className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] w-[400px] text-[#858C95] text-sm font-normal"
-              >
-                <option value="volvo">Select supplier</option>
-                <option value="saab">Rick Grimes</option>
-                <option value="mercedes">Kelvin</option>
-                <option value="audi">Aneal</option>
-              </select>
-            </div>
-
-            <hr className="my-7" />
-
-            {/* Products Details */}
-            <div>
-              <h2 className="text-[#202224] font-bold text-base font-inter">
-                Products Details
-              </h2>
-              {products.map((product, index) => (
-                <div
-                  key={product.id}
-                  className="grid grid-cols-3 gap-5 mt-3 pb-3 mb-3"
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white shadow-custom w-[60%] py-4 px-8 mb-12 rounded-[8px] mt-8 grid gap-y-5 ">
+        <div className="flex items-center gap-4 mb-4">
+          <MoveLeft onClick={onClose} className="cursor-pointer" />
+          <h3 className="font-bold text-2xl font-inter">Add Purchase</h3>
+        </div>
+        <hr />
+        <div className="grid">
+          {/* Purchase Details */}
+          <div>
+            <h2 className="text-[#202224] font-bold text-base font-inter">
+              Purchase Details
+            </h2>
+            <form>
+              <div className="grid gap-y-2 mt-3">
+                <label
+                  htmlFor=""
+                  className="text-[#323539] font-inter font-medium text-sm"
                 >
-                  {/* Select Product */}
-                  <div className="grid gap-y-2">
-                    <label
-                      htmlFor=""
-                      className="text-[#323539] font-inter font-medium text-sm"
-                    >
-                      Select Product
-                    </label>
-                    <select
-                      className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] text-[#858C95] text-sm font-normal"
-                    >
-                      <option value="volvo">Select product</option>
-                      <option value="saab">Saab</option>
-                      <option value="opel">Opel</option>
-                      <option value="audi">Audi</option>
-                    </select>
-                  </div>
-                  {/* Quantity */}
-                  <div className="grid gap-y-2">
-                    <label
-                      htmlFor=""
-                      className="text-[#323539] font-inter font-medium text-sm"
-                    >
-                      Quantity
-                    </label>
-                    <input
-                      type="number"
-                      placeholder="Enter Quantity"
-                      className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] text-[#858C95] text-sm font-normal"
-                    />
-                  </div>
-                  {/* Unit Price */}
-                  <div className="grid gap-y-2">
-                    <label
-                      htmlFor=""
-                      className="text-[#323539] font-inter font-medium text-sm"
-                    >
-                      Unit Price(GHS)
-                    </label>
-                    <input
-                      type="number"
-                      placeholder="Enter Unit Price"
-                      className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] text-[#858C95] text-sm font-normal"
-                    />
-                  </div>
-                </div>
-              ))}
+                  Select Supplier
+                </label>
+                <select className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] w-[400px] text-[#858C95] text-sm font-normal">
+                  <option value="volvo">Select supplier</option>
+                  <option value="saab">Rick Grimes</option>
+                  <option value="mercedes">Kelvin</option>
+                  <option value="audi">Aneal</option>
+                </select>
+              </div>
 
-              <div className="flex items-center justify-between mt-6">
-                <div className="grid">
-                  <h3 className="font-inter text-[#858C95] font-normal text-sm">
-                    TOTAL AMOUNT(GHS)
-                  </h3>
-                  <span className="font-bold text-xl mt-2 font-inter text-[#202224]">
-                    GHS 17,399
-                  </span>
+              <hr className="my-7" />
+
+              {/* Products Details */}
+              <div>
+                <h2 className="text-[#202224] font-bold text-base font-inter">
+                  Products Details
+                </h2>
+                <div className="overflow-y-auto h-[200px] p-3">
+                  {products.map((product, index) => (
+                    <div
+                      key={product.id}
+                      className="grid grid-cols-3 gap-5 mt-3 pb-3 mb-3"
+                    >
+                      {/* Select Product */}
+                      <div className="grid gap-y-2">
+                        <label
+                          htmlFor=""
+                          className="text-[#323539] font-inter font-medium text-sm"
+                        >
+                          Select Product
+                        </label>
+                        <select className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] text-[#858C95] text-sm font-normal">
+                          <option value="volvo">Select product</option>
+                          <option value="saab">Saab</option>
+                          <option value="opel">Opel</option>
+                          <option value="audi">Audi</option>
+                        </select>
+                      </div>
+                      {/* Quantity */}
+                      <div className="grid gap-y-2">
+                        <label
+                          htmlFor=""
+                          className="text-[#323539] font-inter font-medium text-sm"
+                        >
+                          Quantity
+                        </label>
+                        <input
+                          type="number"
+                          placeholder="Enter Quantity"
+                          className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] text-[#858C95] text-sm font-normal"
+                        />
+                      </div>
+                      {/* Unit Price */}
+                      <div className="grid gap-y-2">
+                        <label
+                          htmlFor=""
+                          className="text-[#323539] font-inter font-medium text-sm"
+                        >
+                          Unit Price(GHS)
+                        </label>
+                        <input
+                          type="number"
+                          placeholder="Enter Unit Price"
+                          className="rounded-[4px] py-3 px-2 border border-[#E5E5E7] text-[#858C95] text-sm font-normal"
+                        />
+                      </div>
+                    </div>
+                  ))}
                 </div>
 
-                <div className="grid grid-cols-2 gap-4">
-                  <button
-                    type="button"
-                    onClick={handleAddProduct}
-                    className="border border-[#323539] flex-1 rounded-[4px] py-2 px-8 text-[#323539] font-inter font-semibold text-sm"
-                  >
-                    Add Product
-                  </button>
-                  <button type="submit" className="bg-[#2648EA] rounded-[4px] flex-1 py-2 px-8 text-white font-inter font-semibold text-sm">
-                    Save
-                  </button>
+                <div className="flex items-center justify-between mt-6">
+                  <div className="grid">
+                    <h3 className="font-inter text-[#858C95] font-normal text-sm">
+                      TOTAL AMOUNT(GHS)
+                    </h3>
+                    <span className="font-bold text-xl mt-2 font-inter text-[#202224]">
+                      GHS 17,399
+                    </span>
+                  </div>
+
+                  <div className="grid grid-cols-2 gap-4">
+                    <button
+                      type="button"
+                      onClick={handleAddProduct}
+                      className="border border-[#323539] flex-1 rounded-[4px] py-2 px-8 text-[#323539] font-inter font-semibold text-sm"
+                    >
+                      Add Product
+                    </button>
+                    <button
+                      type="submit"
+                      className="bg-[#2648EA] rounded-[4px] flex-1 py-2 px-8 text-white font-inter font-semibold text-sm"
+                    >
+                      Save
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- components/Inventory/SuppliersTab/SuppliersTab.tsx
+++ components/Inventory/SuppliersTab/SuppliersTab.tsx
@@ -12,7 +12,8 @@ import AddSupplier from "./AddSupplier/AddSupplier";
 
 const SuppliersTab = () => {
   const [searchValues, setSetSearchValues] = useState<string>("");
-  const [showSupplier, setShowSupplier] = useState(false);
+  const [showSupplierModal, setShowSupplierModal] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false); //add supplier modeal
 
   const handleSearchValueChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -21,55 +22,50 @@ const SuppliersTab = () => {
   };
 
   const closeAddSupplier = () => {
-    setShowSupplier(false);
+    setShowSupplierModal(false);
   };
 
   return (
-    <div className="">
-      {showSupplier ? (
-        <AddSupplier onClose={closeAddSupplier}  />
-      ) : suppliersTabTable.length === 0 ? (
-        <div>This is empty</div>
-      ) : (
-        <>
-          <SuppliersTabStats />
-          <div className="p-6 bg-white mt-7 shadow-[6px_6px_54px_0_rgba(0,0,0,0.05)]">
-            <div className="flex items-center justify-between mb-6">
-              <h2 className="text-[#202224] font-semibold text-2xl">
-                Suppliers
-              </h2>
+    <>
+      <div className="">
+        <SuppliersTabStats />
+        <div className="p-6 bg-white mt-7 shadow-[6px_6px_54px_0_rgba(0,0,0,0.05)]">
+          <div className="flex items-center justify-between mb-6">
+            <h2 className="text-[#202224] font-semibold text-2xl">Suppliers</h2>
 
-              <div className="flex items-center gap-3">
-                <SearchFieldInput
-                  value={searchValues}
-                  onChange={handleSearchValueChange}
-                  placeholder="Search for supplier"
-                />
+            <div className="flex items-center gap-3">
+              <SearchFieldInput
+                value={searchValues}
+                onChange={handleSearchValueChange}
+                placeholder="Search for supplier"
+              />
 
-                <Button
-                  type="button"
-                  className="text-white relative flex items-center gap-2 rounded-[12px] font-inter w-[149px]"
-                  variant="secondary"
-                  onClick={() => setShowSupplier(true)}
-                >
-                  <Plus />
-                  Add Supplier
-                </Button>
+              <Button
+                type="button"
+                className="text-white relative flex items-center gap-2 rounded-[12px] font-inter w-[149px]"
+                variant="secondary"
+                onClick={() => setShowSupplierModal(true)}
+              >
+                <Plus />
+                Add Supplier
+              </Button>
 
-                <div className="border p-2  cursor-pointer border-[#494A50] rounded-[12px]">
-                  <SlidersVertical className="text-[#494A50]" />
-                </div>
+              <div className="border p-2  cursor-pointer border-[#494A50] rounded-[12px]">
+                <SlidersVertical className="text-[#494A50]" />
               </div>
             </div>
-            <ExpandableDataTable
-              columns={suppliersTabColumns}
-              data={suppliersTabTable}
-              searchValue={searchValues}
-            />
           </div>
-        </>
-      )}
-    </div>
+          <ExpandableDataTable
+            columns={suppliersTabColumns}
+            data={suppliersTabTable}
+            searchValue={searchValues}
+          />
+        </div>
+      </div>
+
+      {/* Add Supplier Modal */}
+      {showSupplierModal && <AddSupplier onClose={closeAddSupplier} />}
+    </>
   );
 };
 
