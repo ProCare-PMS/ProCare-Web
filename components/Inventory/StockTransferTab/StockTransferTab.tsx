fb361fdded291e8b69ed6d2e969e6074cb4d4064
Aneal Laryea <aneallaryea100@gmail.com>
2024-12-15 05:42:28 +0000

    message

--- components/Inventory/StockTransferTab/StockTransferTab.tsx
+++ components/Inventory/StockTransferTab/StockTransferTab.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import StockTransferTabHeader from "./StockTransferTabHeader";
 import DataTable from "@/components/Tables/data-table";
 import { stockTransferColumns } from "@/components/Tables/stock-transfer-columns";
-import { stockTransferHistoryTable, stockTransferRequestsTable, stockTransferTable } from "@/type";
+import {
+  stockTransferHistoryTable,
+  stockTransferRequestsTable,
+  stockTransferTable,
+} from "@/type";
 import StockTransferHistoryHeader from "./StockTransferHistoryHeader";
 import { stockTransferHistoryColumns } from "@/components/Tables/stock-transfer-history-columns";
 import StockTransferRequestsHeader from "./StockTransferRequestHeader";
@@ -12,7 +16,6 @@ import customAxios from "@/api/CustomAxios";
 import { endpoints } from "@/api/Endpoints";
 
 const StockTransferTab = () => {
-
   const { data: inventoryBranchSyncData } = useQuery({
     queryKey: ["inventoryBranchSync"],
     queryFn: async () =>
@@ -20,28 +23,17 @@ const StockTransferTab = () => {
     select: (findData) => findData?.data?.results,
   });
 
-  console.log(inventoryBranchSyncData)
-
+  console.log(inventoryBranchSyncData);
 
   return (
     <div className="">
-      {/* 
-      <div className="bg-white mt shadow-[6px_6px_54px_0_rgba(0,0,0,0.05)] p-6 rounded-[8px]">
-        <StockTransferTabHeader />
-        <DataTable columns={stockTransferColumns} data={stockTransferTable} />
-      </div>
-      */}
-      {/* Stock Transfer History Table 
-      <div className="bg-white mt shadow-[6px_6px_54px_0_rgba(0,0,0,0.05)] p-6 rounded-[8px]">
-        <StockTransferHistoryHeader />
-        <DataTable columns={stockTransferHistoryColumns} data={stockTransferHistoryTable} />
-      </div>
-      */}
       <div className="bg-white mt shadow-[6px_6px_54px_0_rgba(0,0,0,0.05)] p-6 rounded-[8px]">
         <StockTransferRequestsHeader />
-        <DataTable columns={stockTransferRequestColumns} data={stockTransferRequestsTable} />
+        <DataTable
+          columns={stockTransferRequestColumns}
+          data={inventoryBranchSyncData}
+        />
       </div>
-
     </div>
   );
 };
