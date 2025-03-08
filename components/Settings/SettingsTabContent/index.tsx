a13aa7fd7f0e09ca7f763bb44597f3aab31f0053
Aneal Laryea <aneallaryea100@gmail.com>
2025-03-08 17:45:12 +0000

    update manager account to have billing

--- components/Settings/SettingsTabContent/index.tsx
+++ components/Settings/SettingsTabContent/index.tsx
@@ -81,7 +81,7 @@ export default function SettingsTabsContent() {
                 sx={{ textTransform: "none" }}
               />
             )}
-            {!(user?.is_mca || user?.is_pharmacist || user?.is_manager) && (
+            {!(user?.is_mca || user?.is_pharmacist || !user?.is_manager) && (
               <Tab
                 className="font-inter text-sm font-semibold text-[#858C95]"
                 label="Billing"
