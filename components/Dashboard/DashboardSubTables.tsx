8c6f863d35a1ae5f2d20ae9776668b7db0dd98bb
Marvinkwame <marvinkudjo@gmail.com>
2025-03-25 01:58:10 +0000

    Fixed responsive table issue on the dashboard

--- components/Dashboard/DashboardSubTables.tsx
+++ components/Dashboard/DashboardSubTables.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { ExternalLink, ChevronLeft, ChevronRight } from "lucide-react";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { dashboardSubTables } from "@/type";
 
@@ -16,7 +23,11 @@ const DashboardSubTables = ({ title, data, isLoading }: TableProps) => {
   const itemsPerPage = 5;
 
   const formatDate = (dateString: string) =>
-    new Intl.DateTimeFormat("en-US", { year: "2-digit", month: "2-digit", day: "2-digit" }).format(new Date(dateString));
+    new Intl.DateTimeFormat("en-US", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+    }).format(new Date(dateString));
 
   // Calculate total pages
   const totalPages = Math.ceil(data?.length / itemsPerPage);
@@ -41,9 +52,11 @@ const DashboardSubTables = ({ title, data, isLoading }: TableProps) => {
   };
 
   return (
-    <div className="bg-white p-6 rounded-xl w-[450px] h-[498px] flex-1 flex flex-col">
+    <div className="bg-white p-6 rounded-xl w-[380px] md:w-[450px] h-[498px] flex-1 flex flex-col">
       <div className="flex items-center justify-between mb-6">
-        <h2 className="text-2xl font-medium font-nunito_sans text-gray-900">{title}</h2>
+        <h2 className="text-2xl font-medium font-nunito_sans text-gray-900">
+          {title}
+        </h2>
         {/* 
         <Link href="/inventory?tab=4" className="text-blue-600 font-inter flex items-center gap-1 font-semibold text-sm">
           Open <ExternalLink className="text-blue-600" />
@@ -51,65 +64,85 @@ const DashboardSubTables = ({ title, data, isLoading }: TableProps) => {
         */}
       </div>
 
-      <Table>
-        <TableHeader>
-          <TableRow className="bg-gray-100 rounded-lg">
-            <TableHead className="w-[180px] font-nunito_sans text-gray-900 font-bold">Product Name</TableHead>
-            <TableHead className="text-gray-900 font-nunito_sans font-bold">No. Remaining</TableHead>
-            <TableHead className="w-[150px] text-gray-900 font-nunito_sans font-bold">Expiry Date</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {isLoading ? (
-            Array.from({ length: 5 }).map((_, index) => (
-              <TableRow key={index}>
-                {[32, 16, 24].map((width, i) => (
-                  <TableCell key={i}>
-                    <div className={`h-4 bg-gray-200 rounded w-${width} animate-pulse`} />
-                  </TableCell>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow className="bg-gray-100 rounded-lg">
+              <TableHead className="w-[180px] font-nunito_sans text-gray-900 font-bold">
+                Product Name
+              </TableHead>
+              <TableHead className="text-gray-900 font-nunito_sans font-bold">
+                No. Remaining
+              </TableHead>
+              <TableHead className="w-[150px] text-gray-900 font-nunito_sans font-bold">
+                Expiry Date
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {isLoading
+              ? Array.from({ length: 5 }).map((_, index) => (
+                  <TableRow key={index}>
+                    {[32, 16, 24].map((width, i) => (
+                      <TableCell key={i}>
+                        <div
+                          className={`h-4 bg-gray-200 rounded w-${width} animate-pulse`}
+                        />
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              : data?.length
+              ? getCurrentPageItems()?.map((invoice) => (
+                  <TableRow key={invoice?.name}>
+                    <TableCell className="font-semibold text-sm font-inter text-gray-900">
+                      {invoice?.name}
+                    </TableCell>
+                    <TableCell className="text-sm font-medium font-nunito_sans text-gray-900">
+                      {invoice?.quantity}
+                    </TableCell>
+                    <TableCell className="font-semibold text-sm font-nunito_sans text-gray-900">
+                      {formatDate(invoice?.expiry_date)}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : Array.from({ length: 5 }).map((_, index) => (
+                  <TableRow key={index}>
+                    <TableCell className="text-center font-medium text-gray-900">
+                      -
+                    </TableCell>
+                    <TableCell className="text-center font-medium text-gray-900">
+                      -
+                    </TableCell>
+                    <TableCell className="text-center font-medium text-gray-900">
+                      -
+                    </TableCell>
+                  </TableRow>
                 ))}
-              </TableRow>
-            ))
-          ) : data?.length ? (
-            getCurrentPageItems()?.map((invoice) => (
-              <TableRow key={invoice?.name}>
-                <TableCell className="font-semibold text-sm font-inter text-gray-900">{invoice?.name}</TableCell>
-                <TableCell className="text-sm font-medium font-nunito_sans text-gray-900">{invoice?.quantity}</TableCell>
-                <TableCell className="font-semibold text-sm font-nunito_sans text-gray-900">{formatDate(invoice?.expiry_date)}</TableCell>
-              </TableRow>
-            ))
-          ) : (
-            Array.from({ length: 5 }).map((_, index) => (
-              <TableRow key={index}>
-                <TableCell className="text-center font-medium text-gray-900">-</TableCell>
-                <TableCell className="text-center font-medium text-gray-900">-</TableCell>
-                <TableCell className="text-center font-medium text-gray-900">-</TableCell>
-              </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
+          </TableBody>
+        </Table>
+      </div>
 
       {/* Pagination Controls */}
       <div className="mt-auto flex justify-between items-center pt-4">
-        <Button 
-          variant="outline" 
-          size="sm" 
-          onClick={handlePrevPage} 
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={handlePrevPage}
           disabled={currentPage === 1}
-          className="flex items-center"
+          className="flex items-center "
         >
           <ChevronLeft className="mr-1 h-4 w-4" /> Previous
         </Button>
-        <span className="text-sm text-gray-500">
+        <span className="text-sm text-center text-gray-500">
           Page {currentPage} of {totalPages}
         </span>
-        <Button 
-          variant="outline" 
-          size="sm" 
-          onClick={handleNextPage} 
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={handleNextPage}
           disabled={currentPage === totalPages}
-          className="flex items-center"
+          className="flex items-center "
         >
           Next <ChevronRight className="ml-1 h-4 w-4" />
         </Button>
